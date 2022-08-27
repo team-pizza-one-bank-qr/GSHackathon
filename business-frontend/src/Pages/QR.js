5d2b76ccebcbeecda1a36a4a8edb7e56267c1ccc
Jaylin0312 <jieling.lee.2021@smu.edu.sg>
2022-08-27 17:09:21 +0800

    Revert "Merge branch 'main' of https://github.com/team-pizza-one-bank-qr/GSHackathon"
    This reverts commit 4853a99826416a8987028cc00187f713f4e87ca8, reversing
    changes made to 43df23e77ef25b13c1ed8167a16936263a7efe5a.

--- business-frontend/src/Pages/QR.js
+++ business-frontend/src/Pages/QR.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDOM from "react-dom";
-import QRCode from "react-qr-code";
 
 class QR extends React.Component {
   render() {
     return (
         <div>
-          <center><QRCode value="heyiii" /></center>
+          <h2>QR Page</h2>
         </div>
     );
   }
