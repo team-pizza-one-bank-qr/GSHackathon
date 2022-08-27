11c1a68d930f8dff8d07648723a2363de890c921
Jaylin0312 <jieling.lee.2021@smu.edu.sg>
2022-08-27 18:35:31 +0800

    Styling QR page

--- business-frontend/src/Pages/QR.js
+++ business-frontend/src/Pages/QR.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import ReactDOM from "react-dom";
 import QRCode from "react-qr-code";
+import "./QR.css"
 
-class QR extends React.Component {
-  render() {
+const QR = () => {
     return (
-        <div>
-          <QRCode value='https://qh2yo2dvoh.execute-api.ap-southeast-1.amazonaws.com/qr'/>
+      <div class="body pt-5 pb-5">
+          <div class="container d-flex justify-content-between">
+            <div>
+              <h1 class="mb-5">Welcome to oneBankQR</h1>
+              <h3 class="mb-3">Scan the QR Code now to establish your financial relationship!</h3>
+              <button class="btn btn-dark">
+                Click the button to establish a relationship
+              </button>
+            </div>
+            <div class="card p-5">
+              <div class="cardbody">
+              <QRCode class="mb-3" value='https://qh2yo2dvoh.execute-api.ap-southeast-1.amazonaws.com/qr'/>
+              <div>
+                Scan the QR Code using your native bank App!
+              </div>
+              </div>
+            </div>
+          </div>
         </div>
     );
-  }
 }
 
 export default QR;
