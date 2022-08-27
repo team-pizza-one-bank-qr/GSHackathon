51a2f40a55ec441ed4f546b41fc1e5aff06c8c42
Jaylin0312 <jieling.lee.2021@smu.edu.sg>
2022-08-28 01:37:35 +0800

    building client pop up

--- client-frontend/src/App.js
+++ client-frontend/src/App.js
@@ -1,33 +1,51 @@
 import React from "react";
 import "./App.css";
 
-
 const App = () => {
+  const [showModal, setShowModal] = React.useState(true);
+  const queryParams = new URLSearchParams(window.location.search);
+  const business_id = queryParams.get("business_id");
+  console.log(business_id);
   return (
-    <div class="body container">
-      <div class="modal card" tabIndex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <div class="modal-title">Establishing Connection</div>
-            </div>
-            <div class="modal-body">
-              <p>Are you sure you want to establish a relationship with the current Business
-                using oneBankQR?
-              </p>
-            </div>
-            <div class="modal-footer">
-              <button class="btn btn-primary">
-                Yes
-              </button>
-              <button type="button" class="btn btn-primary btn-dark">
-                No
-              </button>
+    <>
+      {showModal ? (
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-4 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Establish Relationship
+                  </h3>
+                </div>
+                <div className="relative p-6 flex-auto">
+                  <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                    Are you sure you want to proceed to establish the financial Relationship
+                    with business id <span className="text-red">{business_id}</span> ? By Agreeing,
+                    the process is non-revertible. 
+                    <div className="mt-4">Click no if you wish to stop this process.</div>
+                  </p>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-4 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}>
+                    Close
+                  </button>
+                  <button
+                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}>
+                    Agree
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-    </div>
+        
+      ) : null}
+    </>
   );
 };
 
