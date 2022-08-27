622d3fd5bca876b20de0c0f2ace554c6a212ee09
Jaylin0312 <jieling.lee.2021@smu.edu.sg>
2022-08-27 21:59:00 +0800

    Styling QR Page

--- business-frontend/src/Pages/QR.js
+++ business-frontend/src/Pages/QR.js
@@ -5,19 +5,23 @@ import "./QR.css"
 const QR = () => {
     return (
       <div class="body pt-5 pb-5">
-          <div class="container d-flex justify-content-between">
+          <div class="container d-flex justify-content-between align-items-center">
             <div>
-              <h1 class="mb-5">Welcome to oneBankQR</h1>
-              <h3 class="mb-3">Scan the QR Code now to establish your financial relationship!</h3>
+              <h1 class="mb-4">Welcome to oneBankQR</h1>
+              <h5 class="mb-3">Scan the QR Code now to establish your financial relationship!</h5>
               <button class="btn btn-dark">
                 Click the button to establish a relationship
               </button>
             </div>
             <div class="card p-5">
               <div class="cardbody">
-              <QRCode class="mb-3" value='https://qh2yo2dvoh.execute-api.ap-southeast-1.amazonaws.com/qr'/>
-              <div>
-                Scan the QR Code using your native bank App!
+                <h5>Scan with your bank's app</h5>
+                <div class="mb-3">to register</div>
+                <div class="qr mb-3">
+                <QRCode value='https://qh2yo2dvoh.execute-api.ap-southeast-1.amazonaws.com/qr'/>
+                </div>
+              <div class='text-center'>
+                Use Password to log in instead
               </div>
               </div>
             </div>
