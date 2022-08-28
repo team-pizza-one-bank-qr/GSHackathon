623884f0a73bb955e4e78d0f9141eb1bb9b753e6
Cytan00 <chiyong.tan.2021@scis.smu.edu.sg>
2022-08-28 15:37:45 +0800

    update Qr.js

--- business-frontend/src/Pages/QR.js
+++ business-frontend/src/Pages/QR.js
@@ -1,8 +1,41 @@
-import React from 'react';
+
 import QRCode from "react-qr-code";
 import "./QR.css"
 
+import React, {useState, useEffect} from 'react';
+
+
+// const generateQR = () => {
+//     // 2. Create our *dogImage* variable as well as the *setDogImage* function via useState
+//     // We're setting the default value of dogImage to null, so that while we wait for the
+//     // fetch to complete, we dont attempt to render the image
+//   let [QrImage, setQrImage] = useState(null)
+//     // 3. Create out useEffect function
+//   useEffect(() => {
+//     fetch("https://qh2yo2dvoh.execute-api.ap-southeast-1.amazonaws.com/v1/qr")
+//     .then(response => response.json())
+//         // 4. Setting *dogImage* to the image url that we received from the response above
+//     .then(data => setQrImage(data.data))
+//   },[])
+//   return (
+//     <div className="App">
+//     {QrImage && <QRCode value={QrImage}/>}
+//     </div>
+//   );
+// }
+
+
 const QR = () => {
+  let [QrImage, setQrImage] = useState(null)
+  // 3. Create out useEffect function
+useEffect(() => {
+  fetch("https://qh2yo2dvoh.execute-api.ap-southeast-1.amazonaws.com/v1/qr?")
+  .then(response => response.json())
+  // .then(data=>console.log(data))
+      // 4. Setting *dogImage* to the image url that we received from the response above
+  .then((data) => setQrImage(data.data))
+},[])
+
     return (
       <div class="body pt-5 pb-5">
           <div class="container d-flex justify-content-between align-items-center">
@@ -15,7 +48,9 @@ const QR = () => {
                 <h5>Scan with your bank's app</h5>
                 <div class="mb-3">to register</div>
                 <div class="qr mb-3">
-                <QRCode class="svg" value='https://qh2yo2dvoh.execute-api.ap-southeast-1.amazonaws.com/qr'/>
+                <div className="App">
+    {QrImage && <QRCode value={QrImage}/>}
+    </div>
                 </div>
               <div class='text-center'>
                 Use Password to log in instead
